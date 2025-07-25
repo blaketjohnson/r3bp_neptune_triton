a084152484e774413d23f5ea6f3e742251c614ef
Blake Thomas Johnson <blake.t.johnson-1@ou.edu>
2025-07-25 13:35:56 -0500

    Revove CR3BP_Poincare folder from tracking and add .gitignore

--- CR3BP_Poincare/control.py
+++
@@ -1,9 +0,0 @@
-# This file contains small functions to control code workflow
-import sys
-
-def pause():
-    key = input("Press enter to continue or 'q' to quit.\n")
-    if key == "q":
-        sys.exit()
-    else:
-        pass

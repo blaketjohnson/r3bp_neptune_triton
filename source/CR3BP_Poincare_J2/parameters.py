cafe428b710e23d66102eccff8aa7130d83e08e7
Blake Thomas Johnson <blake.t.johnson-1@ou.edu>
2025-08-06 21:42:49 -0500

    test runs on Poincare Maps

--- source/CR3BP_Poincare_J2/parameters.py
+++ source/CR3BP_Poincare_J2/parameters.py
@@ -9,25 +9,25 @@ Author: Blake T. Johnson
 Thesis Project
 """
 # === Jacobi Constant Sweep ===
-C0 = 2.9000       # Initial Jacobi constant
-CF = 2.9000      # Final Jacobi constant
-dC = 0.1000       # Jacobi constant increment
+C0 = 3.0000       # Initial Jacobi constant
+CF = 3.0000      # Final Jacobi constant
+dC = 0.0500       # Jacobi constant increment
 
 # === x0 Sweep Range (Non-Dimensional) ===
-XI = -0.2       # Initial x0 (non-dimensional)
-XF = 1.2       # Final x0 (non-dimensional)
-DX = 0.1000       # Step size for x0 (non-dimensional)
+XI = 0.480       # Initial x0 (non-dimensional)
+XF = 0.520       # Final x0 (non-dimensional)
+DX = 0.0200       # Step size for x0 (non-dimensional)
 
 # === Minimum safe distances from each body (km) ===
 min_distance_neptune_km = 0.0   # Above surface
 min_distance_triton_km  = 0.0   # Above surface
 
 # === Initial y0 Value (Non-Dimensional) ===
-YI = 0.0         # Default: 0.0 (can set to sqrt(3)/2 for L4/L5
+YI = 0.866025         # Default: 0.0 (can set to sqrt(3)/2 for L4/L5
 
 # === Time Parameters (Dimensional) ===
 tlim_sec = 20000.0   # Integration time limit in seconds
-dt_sec = 10.0      # Integration timestep in seconds
+dt_sec = 5.0      # Integration timestep in seconds
 
 
 # === Simulation Flags ===
